ae49cef31199c7a34041cf173ed6257fe1bd1930
AkshayNagdiya <131447025+AkshayNagdiya@users.noreply.github.com>
2023-04-25 01:39:16 +0530

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,8 @@
+alert('hello!')
+let firstName=prompt("write your name:");
+let weight=prompt(" welcome"+" "+firstName+" "+"what is your weight?");
+let height=prompt("what is your height in meters");
+var deno=height*height;
+var BMI= weight/deno;
+alert("Your BMI is "+" "+BMI+" "+"according to given data")
+
